fed08f9a62054db9a2022bf63c9898553bb853ac
maany <imptodefeat@gmail.com>
2018-09-24 12:53:02 +0200

    remove vcsrepo from base profile

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,7 +1,6 @@
 class profile::base {
     include 'docker'
     include 'git'
-    include 'vcsrepo'
     include simple_grid::base
     # provide access to augmented site level configuration file. Write a puppet function to involve yaml compiler
 }
