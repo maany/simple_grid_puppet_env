d5e64b01d94d725c067d3c4e97d1fdbb1c9baf7a
maany <imptodefeat@gmail.com>
2018-10-19 15:10:14 +0200

    install python in profile::base

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,4 +1,9 @@
 class profile::base {
     include 'docker'
     include 'git'
+    class {'python':
+        version    => 'system',
+        pip        => 'present',
+        virtualenv => 'present'
+    }
 }
